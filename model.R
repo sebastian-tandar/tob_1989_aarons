9ef791225305ffa8cc5ff98e2e613cc9bff9e1df
sebastian-tandar <71292919+sebastian-tandar@users.noreply.github.com>
2024-01-01 20:19:03 +0100

    Initiate model - 2 compartments

---
+++ model.R
@@ -0,0 +1,31 @@
+# LIBRARIES----------
+library(rxode2)
+
+# SUPPORTING FUNCTIONS---------
+# Cited creatinine clearance translation from steady-state plasma creatinine concentration
+creatinineCLModel <- function(age, bw, C_creatinine){
+  # creatinine clearance estimation by Cockroft and Gault (1976; cited by Aarons in the core source publication)
+  (150 - age) * bw / C_creatinine
+}
+
+# Conversion function for %BSV/%IIV to eta values
+convertBSVtoETA <- function(individual_variability){
+  log(1+(individual_variability^2))
+}
+
+# INITIAL STATE -----------
+initialState <- c(m_central = 0, m_peripheral = 0)
+
+# MODEL OBJECT # --------------
+ab_amn_1989_tob_aarons <- rxode2({
+  # pre-calculation
+  V_central = V_ratio * exp(eta_V_ratio) * BW 
+  k_elimination = (CL_sigma * exp(eta_CL_sigma) * CL_creatinine) / V_central
+  
+  # ODEs
+  d/dt(m_central) = -(k_elimination + k12) * m_central + k21 * m_peripheral
+  d/dt(m_peripheral) = k12 * m_central - k21 * m_peripheral
+  
+  # post-calculation
+  C_central = m_central / V_central
+})
